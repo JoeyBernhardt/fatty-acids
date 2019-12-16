528af2d328e036d1139f4687efcf0777106ae87d
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2019-12-16 10:52:49 +0100

    more figures

--- R-scripts/05_bird_data.R
+++ R-scripts/05_bird_data.R
@@ -14,3 +14,12 @@ bird <- read_csv("data-raw/Bird-Diet-Data.csv") %>%
 bird %>% 
 	ggplot(aes(x = reorder(taxa, env_count_mean), y = env_count_mean)) + geom_bar(stat = "identity")
 ggsave("figures/bird-count-abundance.png", width = 10, height = 6)
+
+
+### count vs. abundance
+
+bird %>% 
+	ggplot(aes(x = env_count_mean, y = env_biomass_mean)) + geom_point()
+ggsave("figures/bird-count-vs-biomass.png", width = 10, height = 6)
+
+
