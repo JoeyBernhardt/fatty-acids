2337adc9387e0b227deaca53ba3894ad636074bc
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2020-05-01 10:44:24 -0400

    a couple more cleaning changes

--- R-scripts/01_data_extraction.R
+++ R-scripts/01_data_extraction.R
@@ -10,28 +10,8 @@ library(cowplot)
 theme_set(theme_cowplot())
 
 
-dataset4_raw <- read_excel("data-raw/4-DHA_percent_metanalysis.xlsx") %>% 
-	clean_names() %>% 
-	rename(trophic_position = id) %>% 
-	rename(class = phyto_class) %>% 
-	mutate(ecosystem = str_to_lower(ecosystem)) %>%
-	rename(epa = percent_20_5n3_epa) %>% 
-	rename(dha = percent_22_6n3_dha) %>% 
-	mutate(group = ifelse(trophic_position == "Algae", "primary_producer", "consumer")) %>% 
-	select(ecosystem, group, epa, dha, taxa) %>% 
-	mutate(source_dataset = "4") %>% 
-	mutate(dha = str_replace(dha, "n/a", "NA")) %>% 
-	mutate(dha = str_replace_na(dha, replacement = NA)) %>% 
-	mutate(dha = as.numeric(dha))
 
-dataset2_raw <- read_excel("data-raw/2-Columbo-et-al_Marine_and_Terrestrial_PUFA_data_set.xlsx") %>% 
-	clean_names() %>% 
-	mutate(group = ifelse(functional_group == "Phytoplankton", "primary_producer", "consumer")) %>%  
-	mutate(ecosystem = str_to_lower(habitat)) %>% 
-	select(ecosystem, group, epa, dha, genus, species) %>% 
-	mutate(source_dataset = "2")
 
-unique(dataset2_raw$functional_group)
 
 dataset1a_raw <- read_excel("data-raw/1a-Wang_et_al_data.xlsx") %>% 
 	clean_names() %>% 
@@ -49,6 +29,28 @@ dataset1b_raw <- read_excel("data-raw/1b-Adtnl-Raw-Data-from-Wang-et-al-2016.xls
 	select(ecosystem, group, epa, dha) %>% 
 	mutate(source_dataset = "1b")
 
+
+	dataset2_raw <- read_excel("data-raw/2-Columbo-et-al_Marine_and_Terrestrial_PUFA_data_set.xlsx") %>% 
+	clean_names() %>% 
+	mutate(group = ifelse(functional_group == "Phytoplankton", "primary_producer", "consumer")) %>%  
+	mutate(ecosystem = str_to_lower(habitat)) %>% 
+	select(ecosystem, group, epa, dha, genus, species) %>% 
+	mutate(source_dataset = "2")
+
+
+dataset4_raw <- read_excel("data-raw/4-DHA_percent_metanalysis.xlsx") %>% 
+	clean_names() %>% 
+	rename(trophic_position = id) %>% 
+	rename(class = phyto_class) %>% 
+	mutate(ecosystem = str_to_lower(ecosystem)) %>%
+	rename(epa = percent_20_5n3_epa) %>% 
+	rename(dha = percent_22_6n3_dha) %>% 
+	mutate(group = ifelse(trophic_position == "Algae", "primary_producer", "consumer")) %>% 
+	select(ecosystem, group, epa, dha, taxa) %>% 
+	mutate(source_dataset = "4") %>% 
+	mutate(dha = str_replace(dha, "n/a", "NA")) %>% 
+	mutate(dha = str_replace_na(dha, replacement = NA)) %>% 
+	mutate(dha = as.numeric(dha))
 
 dataset5_raw <- read_csv("data-raw/5-Oikos-Twining-2016-FA-Review-Dryad-Data.csv") %>% 
 	clean_names() %>% 
@@ -198,34 +200,105 @@ dataset28_raw <- read_csv("data-raw/28-Kakela-Hyvarinen-1996b.csv") %>%
 	mutate(source_dataset = "28") %>% 
 	mutate(dha = ifelse(dha == "trace", 0, dha)) %>% 
 	mutate(epa = ifelse(epa == "trace", 0, epa)) %>% 
-	filter(!is.na(dha))
+	filter(!is.na(dha)) %>% 
+	mutate(dha = as.numeric(dha)) %>% 
+	mutate(epa = as.numeric(epa))
 
 
 all_data <- bind_rows(dataset2_raw, dataset4_raw, dataset5_raw, dataset1a_raw, dataset1b_raw,
 					  dataset6_raw, dataset7_raw, dataset15_raw,
 					  dataset16_raw, dataset19_raw, dataset22_raw,
 					  dataset20_raw, dataset17_raw, dataset21_raw, dataset23_raw,
-					  dataset24_raw, dataset25_raw, dataset26_raw, dataset27_raw)
+					  dataset24_raw, dataset25_raw, dataset26_raw, dataset27_raw, dataset28_raw)
 
 
+all_data %>% 
+	filter(source_dataset == "4") %>% View
 
+View(unique(all_data$source_dataset)) ### missing 3, 8, 9, 10, 11, 12, 13, 14, 18 
 
 write_csv(all_data, "data-processed/all_data.csv")
 
+all_data <- read_csv("data-processed/all_data.csv")
+
 unique(all_data$source_dataset)
 
+#### get higher order taxonomy
+
+library(taxize)
+
 all_data <- read_csv("data-processed/all_data.csv") %>% 
-	mutate(group = ifelse(group == "primary producer", "primary_producer", group))
+	mutate(group = ifelse(group == "primary producer", "primary_producer", group)) %>% 
+	mutate(genus_species = paste(genus, species, sep = " "))
+
+all_data <- read_csv("data-processed/all_data.csv") %>% 
+	mutate(group = ifelse(group == "primary producer", "primary_producer", group)) %>% 
+	mutate(genus_species = paste(genus, species, sep = " ")) %>% 
+	gather(key = nutrient, value = concentration, epa, dha) %>% 
+	filter(!is.na(concentration)) %>% 
+	mutate(genus_species = ifelse(source_dataset == 4, taxa, genus_species))
+
+all_data %>% 
+	filter(source_dataset == 4) %>% View
+	filter(is.na(species)) %>% View
+
+specieslist <- unique(all_data$genus_species)
+
+hot <- classification(specieslist, db = 'itis')
+hot[1]
+
+str(hot)
+
+hot %>% 
+	map(c("name")) 
+
+map_dfr(hot, extract, c("name", "rank"))
+
+library(myTAI)
+taxonomy(organism = "Nitzschia paleacea" , db = "ncbi", output = "classification" )
+
+outputlst <- apply(specieslist, 1, function(x) taxonomy(organism = x , db = "ncbi", output = "classification"))
+
+outputlst <- hot
+
+# Parse out the taxonomy levels that you require
+taxdata <- data.frame()
+for(x in 1:length(outputlst)){
+	tryCatch({
+		kingdom=filter(outputlst[[x]], rank =="kingdom")$name
+		phylum=filter(outputlst[[x]], rank =="phylum")$name
+		class=filter(outputlst[[x]], rank =="class")$name
+		order=filter(outputlst[[x]], rank =="order")$name
+		family=filter(outputlst[[x]], rank =="family")$name
+		genus=filter(outputlst[[x]], rank =="genus")$name
+		species=filter(outputlst[[x]], rank =="species")$name
+		
+		row <- data.frame(cbind(kingdom = kingdom, phylum=phylum,class=class,order=order,family=family,genus=genus, species = species))
+		taxdata <- bind_rows(taxdata, row)    
+	}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+
+
+
+
+
+
+
+
 
 all_data %>% 
 	filter(!is.na(ecosystem)) %>% 
+	ungroup() %>% 
 	mutate(ecosystem = ifelse(ecosystem == "littoral", "freshwater", ecosystem)) %>% 
-	ggplot(aes(x = dha, fill = ecosystem)) + geom_histogram() +
-	facet_grid(ecosystem ~ group, scales = "free_y")
+	filter(nutrient == "dha") %>% 
+	filter(ecosystem == "marine_or_freshwater") %>% 
+	ggplot(aes(x = concentration, fill = ecosystem)) + geom_histogram() +
+	facet_grid(group ~ ecosystem, scales = "free_y") + ylab("Number of species") + xlab("DHA %")
 
-ggsave("figures/all-dha-ecosystems.pdf", width = 10, height = 8)
+ggsave("figures/all-dha-ecosystems.png", width = 12, height = 6)
 
 	all_data %>% 
+	
 	filter(ecosystem == "freshwater", group == "consumer") %>% View
 
 
